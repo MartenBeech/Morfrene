38cfa5b9656a3467b8003190bc7ad69282b72263
MartenBeech <mortenbech22@hotmail.com>
2020-08-03 12:12:53 +0200

    Scene (OpenWorld) and Scene (Battlefield) added. Added GameObjects to Battlefield

---
+++ Morfrene/Assets/Scripts/Card.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public const int SIZE = 20;
+    public static GameObject[] cards = new GameObject[SIZE];
+
+    private void Start()
+    {
+        for (int i = 0; i < SIZE/2; i++)
+        {
+            cards[i] = GameObject.Find("Card0" + i);
+            cards[i + SIZE/2] = GameObject.Find("Card1" + i);
+        }
+    }
+}
---
+++ Morfrene/Assets/Scripts/Deck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Deck : MonoBehaviour
+{
+    public const int SIZE = 2;
+    public static GameObject[] decks = new GameObject[SIZE];
+
+    private void Start()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            decks[i] = GameObject.Find("Deck" + i);
+        }
+    }
+}
