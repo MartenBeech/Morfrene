a4a28acffa1df66b6955382f77823545a950e065
MartenBeech <mortenbech22@hotmail.com>
2020-08-03 12:25:36 +0200

    Moved scripts into corresponding folders

---
+++ Morfrene/Assets/Scripts/Battlefield/Hero.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hero : MonoBehaviour
+{
+    public const int SIZE = 2;
+    public static GameObject[] heroes = new GameObject[SIZE];
+
+    private void Start()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            heroes[i] = GameObject.Find("Hero" + i);
+        }
+    }
+}
